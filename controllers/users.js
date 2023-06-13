a609b9d81c7803b9419b29f8c6ed02dc774ba155
Abhishek T M <abhishektm4342@gmail.com>
2023-06-13 15:42:02 +0530

    Updated Add or Remove friends,get friends

--- controllers/users.js
+++ controllers/users.js
@@ -30,7 +30,7 @@ export const getUserFriends = async (req,res)=>
             {
                 return {_id,firstName,lastName,occupation,location,picturePath};
             }
-        )
+        );
         res.status(200).json(formattedFriends);
     }
     catch(err)
@@ -51,8 +51,28 @@ export const addRemoveFriend = async (req,res)=>
 
         if(user.friends.includes(friendId))
         {
-            user.friends = user.friends.filter()
+            user.friends = user.friends.filter((id)=> id !== friendId);
+            friend.friends = friend.friends.filter((id)=> id !== id);
         }
+        else
+        {
+            user.friends.push(friendId);
+            friend.friends.push(id);
+        }
+        await user.save();
+        await friend.save();
+
+        const friends = await Promise.all(
+            user.friends.map((id)=> User.findById(id))
+        );
+        const formattedFriends = friends.map(
+            ({_id,firstName,lastName,occupation,location,picturePath})=>
+            {
+                return {_id,firstName,lastName,occupation,location,picturePath};
+            }
+        );
+
+        res.status(200).json(formattedFriends);
     }
     catch(err)
     {
